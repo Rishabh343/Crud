8572c59724e8134c4742591cf1f1f09d17dd0106
Rishabh343 <sinharishabhkumar8@gmail.com>
2026-06-29 15:55:26 +0530

    Added CORS

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { router } from "./routes/userRoutes.js";
+import cors from "cors";
 const app = express();
+
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 dotenv.config();
 connectDB();
 const Port = process.env.PORT || 5000;
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/employee", router);
 app.use("/uploads", express.static("uploads"));
 app.get("/", (req, res) => {
